b91dfa310747c70f3bd78d76ae8888d548500574
ikumisato <kinoko7971@gmail.com>
2024-12-23 15:57:48 +0900

    ローディングアニメーションを追加

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
+// ".loader"クラスを持つ要素を取得
+const loader = document.querySelector(".loader");
+
+// ページ読み込み後、2秒後にローダーの非表示処理を開始
+setTimeout(() => {
+  // トランジションを設定し、opacityを1秒かけて0にする
+  loader.style.transition = "opacity 0.8s ease";
+  loader.style.opacity = "0";
+
+  // 1秒後（opacityのトランジションが完了した後）、ローダーを完全に非表示にする
+  setTimeout(() => {
+    loader.style.display = "none";
+  }, 800);
+}, 1500);
+
 // ページトップボタンの要素を取得
-const pageTopButton = document.getElementById("js-page-top");
+const pageTopButton = document.querySelector(".js-page-top");
 
 // クリックイベントをアロー関数で設定
 pageTopButton.addEventListener("click", () => {
@@ -14,31 +29,18 @@ pageTopButton.addEventListener("click", () => {
 const textAll = document.querySelectorAll(".js-text");
 const trigger = document.querySelector(".js-stagger-trigger");
 
-textAll.forEach((textContainer, index) => {
-  // 元のテキストを1文字ずつ分割して、それぞれを `<span>` 要素にラップ
-  const characters = textContainer.textContent.split("");
-  textContainer.innerHTML = ""; // 元のテキストをクリア
-
-  characters.forEach((char) => {
-    const span = document.createElement("span");
-    span.textContent = char;
-    span.style.display = "inline-block"; // アニメーションに必要なブロック要素として扱う
-    textContainer.appendChild(span);
-  });
-
-  const spans = textContainer.querySelectorAll("span");
-
-  // GSAPアニメーション
-  gsap.from(spans, {
-    y: 30, // 下から上へ移動
-    autoAlpha: 0, // 完全に透明から表示
+textAll.forEach((text, index) => {
+  console.log(text);
+  gsap.from(text, {
+    y: 30,
+    autoAlpha: 0,
     duration: 0.4,
     stagger: {
-      each: 0.1, // 1文字ごとの間隔
+      each: 0.3, //各アニメーションの間隔を指定します。
     },
 
     scrollTrigger: {
-      trigger: index === 0 ? trigger : textAll[index - 1], // 最初は trigger、以降は前の text をトリガーに
+      trigger: index === 0 ? trigger : textAll[index - 1], // 最初は trigger、以降は前のtextをトリガーに
       scrub: true,
       markers: true,
       start: "top 60%",
