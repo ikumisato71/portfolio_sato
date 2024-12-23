84c54f6e2ab88638438cb9713684ccb7699cc57e
ikumisato <kinoko7971@gmail.com>
2024-12-23 17:41:48 +0900

    マウスストーカー追加

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-// ".loader"クラスを持つ要素を取得
+// ーーーーーーー
+// ローディングアニメーション
+// ーーーーーーー
 const loader = document.querySelector(".loader");
 
 // ページ読み込み後、2秒後にローダーの非表示処理を開始
@@ -25,7 +27,9 @@ pageTopButton.addEventListener("click", () => {
   });
 });
 
+// ーーーーーーー
 // タイトルの文字の動き
+// ーーーーーーー
 const textAll = document.querySelectorAll(".js-text");
 const trigger = document.querySelector(".js-stagger-trigger");
 
@@ -46,5 +50,32 @@ textAll.forEach((text, index) => {
       start: "top 60%",
       end: "top 30%",
     },
+  });
+});
+
+// ーーーーーーー
+// マウスストーカー
+// ーーーーーーー
+const stalker = document.getElementById("mouse-stalker");
+let hovFlag = false;
+
+// マウスの動きに追従
+document.addEventListener("mousemove", function (e) {
+  stalker.style.transform =
+    "translate(" + e.clientX + "px, " + e.clientY + "px)";
+});
+
+// すべてのリンク要素に対してホバー時のイベントを設定
+const linkElem = document.querySelectorAll("a:not(.no_stick_)");
+
+linkElem.forEach((link) => {
+  link.addEventListener("mouseover", function () {
+    hovFlag = true;
+    stalker.classList.add("is_active");
+  });
+
+  link.addEventListener("mouseout", function () {
+    hovFlag = false;
+    stalker.classList.remove("is_active");
   });
 });
