cfac79c5fd31e472c3746e86d65e64298a1d8d6a
ikumisato <kinoko7971@gmail.com>
2024-12-31 20:53:40 +0900

    MVの矢印などをCSSに変更

--- js/main.js
+++ js/main.js
@@ -125,3 +125,26 @@ const initSwiper = () => {
 window.addEventListener("load", function () {
   initSwiper();
 });
+
+// PAGE WORKS 横スクロール
+const container = document.querySelectorAll(".container");
+const items = document.querySelectorAll(".js-item");
+
+gsap.set(container, {
+  width: items.length * 100 + "%",
+});
+gsap.set(items, {
+  width: 100 / items.length + "%",
+});
+
+gsap.to(".js-item", {
+  xPercent: -100 * (items.length - 1),
+  scrollTrigger: {
+    trigger: container,
+    start: "top top",
+    end: container.clientWidth,
+    pin: true,
+    anticipatePin: 1,
+    scrub: true,
+  },
+});
