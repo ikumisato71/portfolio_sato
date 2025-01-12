14dbe4b08e5e846d870d787044472940cb5e0c11
ikumisato <kinoko7971@gmail.com>
2025-01-12 19:48:50 +0900

    WP化

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer>
+          <!-- <div class="footer__wrapper"> -->
+          <div class="footer__sns">
+            <a href="https://github.com/ikumisato71"
+              ><img
+                class="sns__icon"
+                src="<?php echo get_template_directory_uri(); ?>/img/icon_Githab.svg"
+                alt="Git-habのリンク"
+            /></a>
+            <a href="https://www.instagram.com/ikumi71/"
+              ><img
+                class="sns__icon"
+                src="<?php echo get_template_directory_uri(); ?>/img/icon_Instagram.svg"
+                alt="Instagramのリンク"
+            /></a>
+          </div>
+          <div class="footer__copyright">
+            <p class="footer__copyright--p">
+              All Rights Reserved 2024 &copy;IkumiSato
+            </p>
+          </div>
+          <!-- </div> -->
+        </footer>
+      </section>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/./js/main.js"></script>
+    <!-- jsファイル -->
+    <?php wp_footer(); ?>
+  </body>
+</html>
