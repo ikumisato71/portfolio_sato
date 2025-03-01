ce9d04498bf8848518ed5316bf72ce6f9348c73a
ikumisato <kinoko7971@gmail.com>
2025-03-02 06:12:21 +0900

    スマホの時のhoverを消す

--- index.php
+++ index.php
@@ -461,7 +461,6 @@
     </main>
     <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js"></script>
-
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script src="./js/main.js"></script>
     <!-- jsファイル -->
